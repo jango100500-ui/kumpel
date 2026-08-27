9479ce000350b0162c2223d082808fc9c26312bf
jango100500-ui <jango100500@gmail.com>
2026-08-27 18:25:41 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from './main/App';
 import './index.css';
 
+document.addEventListener('gesturestart', (e) => e.preventDefault());
+document.addEventListener('dblclick', (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
