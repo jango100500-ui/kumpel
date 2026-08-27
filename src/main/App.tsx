b059f65c09656e4215a1f430389cfb12c523363c
jango100500-ui <jango100500@gmail.com>
2026-08-27 16:43:27 +0300

    Update App.tsx

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { AuthScreen } from './AuthScreen';
 
 export const App: React.FC = () => {
-  return (
-    <main className="min-h-screen w-full bg-white flex flex-col justify-between" />
-  );
+  return <AuthScreen />;
 };
